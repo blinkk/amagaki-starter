61a30851eeb1ded2b4c5883693b30aa5d2f466bc
Jeremy Weinstein <jeremydw@gmail.com>
2021-01-17 15:49:40 -0800

    Be quiet on first build

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,41 @@
 const glob = require('glob');
 const WebpackNotifierPlugin = require('webpack-notifier');
 
-module.exports = {
-  entry: ["./src/ts/main.ts"].concat(
-    glob.sync("./src/sass/**/*.sass", { ignore: ["./src/sass/**/_*"] })
-  ),
-  plugins: [new WebpackNotifierPlugin()],
-  module: {
-    // Sass
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "css/",
-              name: "[name].min.css",
+module.exports = env => {
+  return {
+    entry: ['./src/ts/main.ts'].concat(
+      glob.sync('./src/sass/**/*.sass', {ignore: ['./src/sass/**/_*']})
+    ),
+    plugins: env.quiet ? [] : [new WebpackNotifierPlugin()],
+    module: {
+      // Sass
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'css/',
+                name: '[name].min.css',
+              },
             },
-          },
-          "sass-loader",
-        ],
-      },
-      /* TODO: Fix me.
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-        options: {
-          configFile: './tsconfig.json',
+            'sass-loader',
+          ],
         },
-        include: ['./src/ts/'],
-        exclude: /node_modules/,
-      },
-      */
-    ],
-  },
+        /* TODO: Fix me.
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+          options: {
+            configFile: './tsconfig.json',
+          },
+          include: ['./src/ts/'],
+          exclude: /node_modules/,
+        },
+        */
+      ],
+    },
+  };
 };
