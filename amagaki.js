b75ba00df5ac4145903b25578fec2aa63cb750f2
Scott Yano <stmhawaii@gmail.com>
2021-04-07 08:44:35 +0900

    Fix js path.

--- amagaki.js
+++ amagaki.js
@@ -1,6 +1,9 @@
 const placeholderPlugin = require('./plugins/placeholder');
 
 module.exports = function (pod) {
+
+  const nunjucksPlugin = pod.plugins.get('NunjucksPlugin');
+
   pod.configure({
     metadata: {
       name: 'Amagaki Starter',
@@ -14,9 +17,19 @@ module.exports = function (pod) {
         path: '/static/css/',
         staticDir: '/dist/css/',
       },
+      {
+        path: '/static/js/',
+        staticDir: '/dist/js/',
+      },
     ],
   });
+
   placeholderPlugin.register(pod, {
     sizes: ['16x9', '1x1', '9x16', '7x3'],
+  });
+
+
+  nunjucksPlugin.addGlobal('cb', () => {
+    return Math.floor(Date.now() / 1000);
   });
 };
