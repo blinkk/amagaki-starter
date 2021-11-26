c0fa42710ad2d6477f4400cf4693b7fe1c9d7c09
Jeremy Weinstein <jeremydw@gmail.com>
2021-11-26 11:53:02 -0800

    fix: escape values used in template

--- plugins/degu.ts
+++ plugins/degu.ts
@@ -4,37 +4,52 @@
 
 import {NunjucksTemplateEngine, Pod} from '@amagaki/amagaki';
 
-import nunjucks from 'nunjucks';
+import {SafeString} from 'nunjucks/src/runtime';
+import {escape} from 'nunjucks/src/lib';
+
+/** Template tag function for escaping values for use with Nunjucks templates. */
+const html = (literals: TemplateStringsArray, ...substitutions: unknown[]) => {
+  let result = '';
+  for (const [i, substitution] of substitutions.entries()) {
+    // Avoid double-escaping when using ternary or nested expressions.
+    if (substitution instanceof SafeString) {
+      result += literals[i] + substitution;
+    } else {
+      result += literals[i] + escape(substitution);
+    }
+  }
+  result += literals[literals.length - 1];
+  return new SafeString(result.trim());
+};
 
 export class DeguPlugin {
   static register(pod: Pod) {
-    const nunjucksEngine = pod.engines.getEngineByExtension(
+    const engine = pod.engines.getEngineByExtension(
       '.njk'
     ) as NunjucksTemplateEngine;
-    nunjucksEngine.env.addGlobal('degu', {
+    engine.env.addGlobal('degu', {
       asset: DeguPlugin.asset,
     });
   }
 
+  /** Returns a `<degu-image>` or `<degu-video>` depending requested file. */
   static asset(options: {
     url: string;
-    alt: string;
+    altText: string;
     width?: number;
     height?: number;
     class?: string;
   }) {
     // TODO: When `degu-video` is available, check `url` extension and output
     // the appropriate element.
-    return nunjucks.runtime.markSafe(
-      `
-        <degu-image
-          src="${options.url}"
-          alt="${options.alt ?? ''}"
-          ${options.class ? `class="${options.class}"` : ''}
-          ${options.width ? `width="${options.width}"` : ''}
-          ${options.height ? `height="${options.height}"` : ''}
-        ></degu-image>
-      `.trim()
-    );
+    return html`
+      <degu-image
+        src="${options.url}"
+        alt="${options.altText ?? ''}"
+        ${options.class ? html`class="${options.class}"` : ''}
+        ${options.width ? html`width="${options.width}"` : ''}
+        ${options.height ? html`height="${options.height}"` : ''}
+      ></degu-image>
+    `;
   }
 }
