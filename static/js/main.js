1579d96942022df5f4789ffc7c7f73d63c049661
jeremydw <jeremydw@users.noreply.github.com>
2021-11-28 02:07:17 +0000

    deploy: 93e9a75cb8246ae4846c337de733d70bcd1a620f

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,13 @@
-import { DeguImage } from '@blinkk/degu/lib/lit/image';
-window.customElements.define('degu-image', DeguImage);
+import * as dom from '@blinkk/degu/lib/dom/dom';
+import * as is from '@blinkk/degu/lib/is/is';
+console.log('Add JS here');
+class App {
+    test() {
+        if (is.mobile()) {
+            dom.event(document.documentElement, 'test', {});
+        }
+    }
+}
+const app = new App();
+app.test();
 //# sourceMappingURL=main.js.map
