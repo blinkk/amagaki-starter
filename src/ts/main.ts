e98f3b2d28d03f51e6f6bca7d8959e72858fd515
Scott Yano <stmhawaii@gmail.com>
2021-12-01 08:27:30 +0900

    Add degu youtube modal

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,14 +1,23 @@
+import '@github/details-dialog-element';
 import {DeguImage} from '@blinkk/degu/lib/components/image';
 import {DeguVideo} from '@blinkk/degu/lib/components/video';
+import {DeguYouTubeInline} from '@blinkk/degu/lib/components/youtube-inline';
+import {DeguYouTubeModal} from '@blinkk/degu/lib/components/youtube-modal';
 
 window.customElements.define('degu-image', DeguImage);
 window.customElements.define('degu-video', DeguVideo);
+window.customElements.define('degu-youtube-inline', DeguYouTubeInline);
+window.customElements.define('degu-youtube-modal', DeguYouTubeModal);
 
 class App {
   constructor() {
     // Insert global code here.
     // Module-specific code should be implemented using custom elements.
+
+    DeguYouTubeModal.register(document.body);
   }
 }
 
-new App();
+document.addEventListener('DOMContentLoaded', () => {
+  new App();
+});
