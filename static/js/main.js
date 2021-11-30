ed563cd337b4ab68f483e87b89112103cb70038d
jeremydw <jeremydw@users.noreply.github.com>
2021-11-30 04:19:37 +0000

    deploy: 17e9bf0edf335ac4e034385eac338db29b57b41e

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,7 @@
 import { DeguImage } from '@blinkk/degu/lib/lit/image';
+import { DeguVideo } from '@blinkk/degu/lib/lit/video';
 window.customElements.define('degu-image', DeguImage);
+window.customElements.define('degu-video', DeguVideo);
 class App {
     constructor() {
         // Insert global code here.
