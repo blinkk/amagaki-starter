16c0735baa01fc0f5ae4089e16b863c97a6b6a60
Jeremy Weinstein <jeremydw@gmail.com>
2022-01-13 14:51:54 -0800

    feat: add quicklink #19

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,6 +4,8 @@ import {DeguImage} from '@blinkk/degu/lib/components/image';
 import {DeguVideo} from '@blinkk/degu/lib/components/video';
 import {DeguYouTubeInline} from '@blinkk/degu/lib/components/youtube-inline';
 import {DeguYouTubeModal} from '@blinkk/degu/lib/components/youtube-modal';
+// @ts-ignore
+import {listen as quickLinkListen} from 'quicklink';
 
 window.customElements.define('degu-image', DeguImage);
 window.customElements.define('degu-video', DeguVideo);
@@ -14,8 +16,8 @@ class App {
   constructor() {
     // Insert global code here.
     // Module-specific code should be implemented using custom elements.
-
     DeguYouTubeModal.register(document.body);
+    quickLinkListen();
   }
 }
 
