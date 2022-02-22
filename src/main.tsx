d68cec5cdef8ac10b55c57728751d30572e3c48a
Jeremy Weinstein <jeremydw@gmail.com>
2022-02-21 21:40:20 -0800

    feat!: switch to preact for templates

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
+/** @jsx */
 import '@github/details-dialog-element';
 
+import Hero from './partials/Hero/Hero';
 import {DeguImage} from '@blinkk/degu/lib/components/image';
 import {DeguVideo} from '@blinkk/degu/lib/components/video';
 import {DeguYouTubeInline} from '@blinkk/degu/lib/components/youtube-inline';
 import {DeguYouTubeModal} from '@blinkk/degu/lib/components/youtube-modal';
+import {PartialHydrator} from '@amagaki/amagaki-engine-preact/dist/hydrator';
+
 // @ts-ignore
 import {listen as quickLinkListen} from 'quicklink';
 
@@ -12,10 +16,13 @@ window.customElements.define('degu-video', DeguVideo);
 window.customElements.define('degu-youtube-inline', DeguYouTubeInline);
 window.customElements.define('degu-youtube-modal', DeguYouTubeModal);
 
+// Modules that require hydration must be registered.
+PartialHydrator.register({
+  components: [Hero],
+});
+
 class App {
   constructor() {
-    // Insert global code here.
-    // Module-specific code should be implemented using custom elements.
     DeguYouTubeModal.register(document.body);
     quickLinkListen();
   }
