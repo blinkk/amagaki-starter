bcc5305628ddefd8539697eed4b7c8b4ca86a204
Jeremy Weinstein <jeremydw@gmail.com>
2022-02-21 21:29:36 -0800

    chore: use hydrator from preact plugin

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import {DeguImage} from '@blinkk/degu/lib/components/image';
 import {DeguVideo} from '@blinkk/degu/lib/components/video';
 import {DeguYouTubeInline} from '@blinkk/degu/lib/components/youtube-inline';
 import {DeguYouTubeModal} from '@blinkk/degu/lib/components/youtube-modal';
-import {PartialHydrator} from './utils/partials';
+import {PartialHydrator} from '@amagaki/amagaki-engine-preact/dist/hydrator';
 import Hero from './partials/Hero/Hero';
 
 window.customElements.define('degu-image', DeguImage);
@@ -15,9 +15,7 @@ window.customElements.define('degu-youtube-modal', DeguYouTubeModal);
 
 // Modules that require hydration must be registered here.
 PartialHydrator.register({
-  components: {
-    hero: Hero,
-  },
+  components: [Hero],
 });
 
 class App {
